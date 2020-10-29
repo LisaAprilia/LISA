7451e7f73a71beb30743e5bd600762a6a0e684d3
LISA <aprilialisa654@gmail.com>
2020-10-29 07:59:41 +0700

    Lisa Aprilia

---
+++ materi2/materi2/system/resources/views/produk.blade.php
@@ -0,0 +1,145 @@
+@extends ('template.base')
+
+@section('content')
+	<h1>Ini Halaman produk di shopmax Toko ku</h1>
+	<div class="row mb-5">
+
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/prod_2.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Gray Shoe</a></h2>
+                <strong class="item-price">RP.200.000</strong>
+              </div>
+              <div class="col-lg-3 col-md-6 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/prod_3.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Blue Shoe High Heels</a></h2>
+                <strong class="item-price"><del>RP.460.000</del> RP.280.000</strong>
+              </div>
+
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/model_5.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Denim Jacket</a></h2>
+                <strong class="item-price"><del>RP.460.000</del> RP.280.000</strong>
+
+                <div class="star-rating">
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2arning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                </div>
+
+              </div>
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/prod_1.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Leather Green Bag</a></h2>
+                <strong class="item-price"><del>RP.460.000</del> RP.280.000</strong>
+                <div class="star-rating">
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/model_1.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Smooth Cloth</a></h2>
+                <strong class="item-price"><del>RP.460.00</del> RP.280.000</strong>
+              </div>
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/model_7.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Yellow Jacket</a></h2>
+                <strong class="item-price">RP.580.000</strong>
+              </div>
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/prod_2.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Gray Shoe</a></h2>
+                <strong class="item-price">RP.200.000</strong>
+              </div>
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/prod_3.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Blue Shoe High Heels</a></h2>
+                <strong class="item-price"><del>RP.460.000</del> RP.280.000</strong>
+              </div>
+
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/model_5.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Denim Jacket</a></h2>
+                <strong class="item-price"><del>RP.460.000</del> RP.280.000</strong>
+
+                <div class="star-rating">
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                </div>
+
+              </div>
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/prod_1.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Leather Green Bag</a></h2>
+                <strong class="item-price"><del>RP.460.000</del> RP.280.000</strong>
+                <div class="star-rating">
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                  <span class="icon-star2 text-warning"></span>
+                </div>
+              </div>
+
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/model_1.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Smooth Cloth</a></h2>
+                <strong class="item-price"><del>RP.460.000</del> RP.280.000</strong>
+              </div>
+              <div class="col-lg-3 col-md-2 item-entry mb-2">
+                <a href="#" class="product-item md-height bg-gray d-block">
+                  <img src="{{url('public')}}/images/model_7.png" alt="Image" class="img-fluid">
+                </a>
+                <h2 class="item-title"><a href="#">Yellow Jacket</a></h2>
+                <strong class="item-price">RP.580.000</strong>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12 text-center">
+                <div class="site-block-27">
+                  <ul>
+                    <li><a href="#">&lt;</a></li>
+                    <li class="active"><span>1</span></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">&gt;</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+@endsection
---
+++ materi2/materi2/system/resources/views/promo.blade.php
@@ -0,0 +1,20 @@
+@extends ('template.base')
+
+@section('content')
+  <h1><P align=center>PROMO</P></h1>
+  <p align=center >s<div class="row mb-5"><footer class="site-footer custom-border-top">
+      <div class="container">
+        <div class="row">
+          <div class="img-fluid">
+            <a href="#" class="block-6">
+              <img src="{{url('public')}}/images/about_1.jpg" alt="Image placeholder"  align=center class="img-fluid rounded mb-4">
+              <p align=center >Promo from DEsember 15 &mdash; 25, 2020</p>
+            </p>
+              </a>
+            </p>
+           </a>
+         </div>
+       </div>
+     </div>
+  </div>
+@endsection
